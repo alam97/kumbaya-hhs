8638e282560e52528a6563693fc10d6b1b4ba892
217852 <217852@edu.p.lodz.pl>
2020-04-06 16:15:28 +0200

    building DataProvider async

--- kumbaya/data/src/main/java/dataproviding/DataProvider.java
+++ kumbaya/data/src/main/java/dataproviding/DataProvider.java
@@ -1,4 +1,81 @@
 package dataproviding;
 
+
+import com.azure.cosmos.*;
+import com.azure.cosmos.models.CosmosAsyncContainerResponse;
+import com.azure.cosmos.models.CosmosAsyncDatabaseResponse;
+import com.azure.cosmos.models.CosmosContainerProperties;
+import com.google.common.collect.Lists;
+import reactor.core.publisher.Mono;
+
 public class DataProvider {
+
+    private final String EndpointUri = "https://malawi-sensing.documents.azure.com:443/";
+    private final String AuthKey = "yBWJ4Nmp8e6WpfY7jFrgdGQ1DdbonOKebQ5M6M1bvpT44tKprKCTZqvbBW2GpNvbru2wKn3vYOxZBbFgBXw2ZQ==";
+
+    private CosmosAsyncClient client;
+    private final String databaseName = "malawi-sensing";
+    private final String containerName = "Range";
+
+    private CosmosAsyncDatabase database;
+    private CosmosAsyncContainer container;
+
+    public void close(){ client.close(); }
+
+    private void connectToDB() throws Exception {
+
+        ConnectionPolicy defaultPolicy = ConnectionPolicy.getDefaultPolicy();
+        //  Setting the preferred location to Cosmos DB Account region
+        //  West US is just an example. User should set preferred location to the Cosmos DB region closest to the application
+        defaultPolicy.setPreferredLocations(Lists.newArrayList("East US"));
+
+        //  Create async client
+        //  <CreateAsyncClient>
+        client = new CosmosClientBuilder()
+                .setEndpoint(EndpointUri)
+                .setKey(AuthKey)
+                .setConnectionPolicy(defaultPolicy)
+                .setConsistencyLevel(ConsistencyLevel.EVENTUAL)
+                .buildAsyncClient();
+
+        //  </CreateAsyncClient>
+
+        createDatabaseIfNotExists();
+        createContainerIfNotExists();
+
+    }
+
+    private void createDatabaseIfNotExists() throws Exception {
+     //   System.out.println("Create database " + databaseName + " if not exists.");
+
+        //  Create database if not exists
+        //  <CreateDatabaseIfNotExists>
+        Mono<CosmosAsyncDatabaseResponse> databaseIfNotExists = client.createDatabaseIfNotExists(databaseName);
+        databaseIfNotExists.flatMap(databaseResponse -> {
+            database = databaseResponse.getDatabase();
+            //System.out.println("Checking database " + database.getId() + " completed!\n");
+            return Mono.empty();
+        }).block();
+        //  </CreateDatabaseIfNotExists>
+    }
+
+    private void createContainerIfNotExists() throws Exception {
+      //  System.out.println("Create container " + containerName + " if not exists.");
+
+        //  Create container if not exists
+        //  <CreateContainerIfNotExists>
+
+        CosmosContainerProperties containerProperties = new CosmosContainerProperties(containerName, "/lastName");
+        Mono<CosmosAsyncContainerResponse> containerIfNotExists = database.createContainerIfNotExists(containerProperties, 400);
+
+        //  Create container with 400 RU/s
+        containerIfNotExists.flatMap(containerResponse -> {
+            container = containerResponse.getContainer();
+          //  System.out.println("Checking container " + container.getId() + " completed!\n");
+            return Mono.empty();
+        }).block();
+
+        //  </CreateContainerIfNotExists>
+    }
+
 }
