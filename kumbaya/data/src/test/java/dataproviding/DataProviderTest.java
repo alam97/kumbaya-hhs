9d68fce78b612f332053bea2373308a77b96448e
217852 <217852@edu.p.lodz.pl>
2020-05-14 23:00:53 +0200

    DataProvider tests pass

---
+++ kumbaya/data/src/test/java/dataproviding/DataProviderTest.java
@@ -0,0 +1,29 @@
+package dataproviding;
+
+import datamodel.Range;
+import datamodel.SoilMeasurement;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+class DataProviderTest {
+
+    @Test
+    void readSoilMeasurement() {
+        DataProvider dataProvider = new DataProvider();
+        dataProvider.connectToDB();
+        SoilMeasurement sm = dataProvider.readSoilMeasurement("user1");
+        dataProvider.close();
+        Assertions.assertEquals("2020-05-06", sm.getMeasureDate());
+    }
+
+    @Test
+    void readRanges() {
+        DataProvider dataProvider = new DataProvider();
+        dataProvider.connectToDB();
+        List<Range> ranges = dataProvider.readRanges();
+        dataProvider.close();
+        Assertions.assertEquals(3, ranges.size());
+    }
+}
