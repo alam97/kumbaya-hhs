71a6990b8f45b63d3bbbb107f45992fa65074d0b
217852 <217852@edu.p.lodz.pl>
2020-05-24 20:05:27 +0200

    resolved errors by adding requires

--- kumbaya/presentation/src/main/java/module-info.java
+++ kumbaya/presentation/src/main/java/module-info.java
@@ -3,6 +3,8 @@ module org.kumbaya.hhs {
     requires javafx.fxml;
     requires javafx.web;
     requires logic;
+    requires com.fasterxml.jackson.annotation;
+    requires com.fasterxml.jackson.core;
     opens org.kumbaya.hhs to javafx.fxml;
     exports org.kumbaya.hhs;
 }
