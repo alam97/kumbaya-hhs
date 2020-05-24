764b95063fe3ee5fb40740df0e4bc99ab33be824
217852 <217852@edu.p.lodz.pl>
2020-05-24 20:09:57 +0200

    measurement fxml + controller

---
+++ kumbaya/presentation/src/main/java/org/kumbaya/hhs/SoilMeasurementTransitionController.java
@@ -0,0 +1,29 @@
+package org.kumbaya.hhs;
+
+import applogic.SoilDefiner;
+import javafx.fxml.FXML;
+
+public class SoilMeasurementTransitionController extends MainViewsSwitcher {
+
+    // will be the measurement changing view once the design is complete
+
+  /*  @FXML
+    private void initialize() throws Exception {
+        System.out.println("Test");
+        setText();
+    }
+
+    private void setText(){
+        SoilDefiner soilDefiner = new SoilDefiner();
+        String output = null;
+        try {
+            output = soilDefiner.defineSoil("user1");
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        System.out.println(output);
+    }*/
+
+
+}
