7404df1f9ce82bac19434df8a81c6a15df9a1079
217852 <217852@edu.p.lodz.pl>
2020-06-05 14:24:47 +0200

    added javafx.media dependency

--- kumbaya/presentation/src/main/java/module-info.java
+++ kumbaya/presentation/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module org.kumbaya.hhs {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.web;
+    requires javafx.media;
     requires logic;
     requires com.fasterxml.jackson.annotation;
     requires com.fasterxml.jackson.core;
