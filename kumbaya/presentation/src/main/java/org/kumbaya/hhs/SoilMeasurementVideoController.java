74cee729ada87b0272a248488bfda37cdfbeba22
217852 <217852@edu.p.lodz.pl>
2020-06-05 15:11:53 +0200

    mediaview player for mp4 file added

--- kumbaya/presentation/src/main/java/org/kumbaya/hhs/SoilMeasurementVideoController.java
+++ kumbaya/presentation/src/main/java/org/kumbaya/hhs/SoilMeasurementVideoController.java
@@ -1,11 +1,19 @@
 package org.kumbaya.hhs;
 
 import javafx.fxml.FXML;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
 import javafx.scene.web.WebView;
 
 import java.io.IOException;
 
 public class SoilMeasurementVideoController extends MainViewsSwitcher {
+
+    @FXML
+    private MediaView mv;
+    private MediaPlayer mp;
+    private Media me;
 
     @FXML
     private void goBacktoSoilMeasurement() throws IOException {
@@ -17,14 +25,12 @@ public class SoilMeasurementVideoController extends MainViewsSwitcher {
 
     @FXML
     private void initialize() {
-        deviceVideoWebView.getEngine().loadContent("<iframe width='775' height='435' src='http://www.youtube.com/embed/4bvrMDpVv2I' />");
-        deviceVideoWebView.setContextMenuEnabled(true);
+       // deviceVideoWebView.getEngine().loadContent("<iframe width='775' height='435' src='http://www.youtube.com/embed/4bvrMDpVv2I' />");
+       // deviceVideoWebView.setContextMenuEnabled(true);
+        me = new Media(this.getClass().getResource("videos/animation.mp4").toString());
+        mp = new MediaPlayer(me);
+        mv.setMediaPlayer(mp);
+        mp.setAutoPlay(true);
         }
 
-
-    @FXML
-    private void switchToMeasuring(){
-
     }
-
-}
