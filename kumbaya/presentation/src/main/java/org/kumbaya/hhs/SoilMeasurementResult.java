3a5d6657aa0715219ac507f2ddf472bb176e4153
217852 <217852@edu.p.lodz.pl>
2020-06-07 15:21:58 +0200

    lambda expressions written to avoid controller definition

--- kumbaya/presentation/src/main/java/org/kumbaya/hhs/SoilMeasurementResult.java
+++ kumbaya/presentation/src/main/java/org/kumbaya/hhs/SoilMeasurementResult.java
@@ -1,13 +1,64 @@
 package org.kumbaya.hhs;
 
 import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
 import java.io.IOException;
 
-public class SoilMeasurementResult extends MainViewsSwitcher {
+public class SoilMeasurementResult {
 
     @FXML
-    private void goBackToSoilMeasurement() throws IOException {
-        App.setRoot("soilmeasurementScene");
+    ImageView result;
+    @FXML
+    ImageView back;
+    @FXML
+    ImageView home;
+
+
+    private String path;
+
+
+
+   public SoilMeasurementResult(String path) {
+        this.path = path;
+    }
+
+    public void initialize() {
+        result.setImage(new Image(getClass().getResource(path).toString()));
+
+        back.setOnMouseClicked((event) -> {    // lambda expression
+            try {
+                App.setRoot("soilmeasurementScene");
+            }
+            catch (IOException e){
+                e.printStackTrace();
+            }
+        });
+        back.setOnTouchPressed((event) -> {    // lambda expression
+            try {
+                App.setRoot("soilmeasurementScene");
+            }
+            catch (IOException e){
+                e.printStackTrace();
+            }
+        });
+        home.setOnMouseClicked((event) -> {    // lambda expression
+            try {
+                App.setRoot("mainScene");
+            }
+            catch (IOException e){
+                e.printStackTrace();
+            }
+        });
+        home.setOnTouchPressed((event) -> {    // lambda expression
+            try {
+                App.setRoot("mainScene");
+            }
+            catch (IOException e){
+                e.printStackTrace();
+            }
+        });
     }
 
 }
