e8473cf2667bca97648937313488faf0eb5106e3
217852 <217852@edu.p.lodz.pl>
2020-06-08 18:28:09 +0200

    Fertilizer List Controller

---
+++ kumbaya/presentation/src/main/java/org/kumbaya/hhs/FertilizerListController.java
@@ -0,0 +1,16 @@
+package org.kumbaya.hhs;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class FertilizerListController {
+
+    @FXML
+    TableView table;
+    TableColumn image;
+    TableColumn name;
+
+
+
+}
