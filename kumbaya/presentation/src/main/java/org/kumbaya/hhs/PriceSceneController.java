40618a7ed032b2e5a8d5f9203373a1898ef261bf
217852 <217852@edu.p.lodz.pl>
2020-06-08 14:04:30 +0200

    cleaned content, code in FXML

--- kumbaya/presentation/src/main/java/org/kumbaya/hhs/PriceSceneController.java
+++ kumbaya/presentation/src/main/java/org/kumbaya/hhs/PriceSceneController.java
@@ -1,19 +1,4 @@
 package org.kumbaya.hhs;
 
-import javafx.fxml.FXML;
-import javafx.scene.text.Text;
-
-public class PriceSceneController extends MainViewsSwitcher {
-
-    @FXML
-    Text SweetPotatoPrice;
-    @FXML
-    Text MaizePrice;
-    @FXML
-    Text SoyBeansPrice;
-
-    @FXML
-    private void initialize() {
-
-    }
+public class PriceSceneController extends MainChoice {
 }
