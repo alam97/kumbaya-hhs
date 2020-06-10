5f648673b3ce07b453c60fec43108a4a041845ec
217852 <217852@edu.p.lodz.pl>
2020-06-10 13:51:44 +0200

    final and static variables for memory management

--- kumbaya/data/src/main/java/dataproviding/DataProvider.java
+++ kumbaya/data/src/main/java/dataproviding/DataProvider.java
@@ -18,28 +18,27 @@ import java.util.ResourceBundle;
 public class DataProvider {
 
     //region Fields
-    private ResourceBundle bundle = ResourceBundle.getBundle("databasekeys");
-    private final String EndpointUri = bundle.getString("endpoint");
-    private final String AuthKey = bundle.getString("key");
-    private CosmosSyncClient client;
-    private final String databaseName = bundle.getString("databaseName");
-    private CosmosSyncDatabase database;
-    private FeedOptions queryOptions = new FeedOptions();
+    private final static ResourceBundle bundle = ResourceBundle.getBundle("databasekeys");
+    private static CosmosSyncClient client;
+    private static CosmosSyncDatabase database;
+    private final static FeedOptions queryOptions = new FeedOptions();
     //endregion
 
     //region Connect + Close to DB
-    public void close(){ client.close(); }
+    public void close() {
+        client.close();
+    }
 
     public void connectToDB() {
         ConnectionPolicy defaultPolicy = ConnectionPolicy.defaultPolicy();
         client = new CosmosClientBuilder()
-                .endpoint(EndpointUri)
-                .key(AuthKey)
+                .endpoint(bundle.getString("endpoint"))
+                .key(bundle.getString("key"))
                 .connectionPolicy(defaultPolicy)
                 .consistencyLevel(ConsistencyLevel.EVENTUAL)
                 .buildSyncClient();
 
-        database = client.getDatabase(databaseName);
+        database = client.getDatabase(bundle.getString("databaseName"));
 
     }
     //endregion
@@ -47,13 +46,13 @@ public class DataProvider {
     //region Read
 
     public SoilMeasurement readSoilMeasurement(String userid) {
-        List<SoilMeasurement> measurements = new ArrayList<>();
+        List < SoilMeasurement > measurements = new ArrayList < > ();
         queryOptions.populateQueryMetrics(true);
         CosmosSyncContainer container = database.getContainer(bundle.getString("soilmeasurement"));
-        String query = "SELECT TOP 1 * FROM c WHERE c.userid=" + "'" + userid +"'" + " AND c.measureDate <= GetCurrentDateTime() ORDER BY c.measureDate DESC";
-        Iterator<FeedResponse<CosmosItemProperties>> responseIterator = container.queryItems(query, queryOptions);
-        responseIterator.forEachRemaining( response -> {
-            response.results().forEach( item -> {
+        String query = "SELECT TOP 1 * FROM c WHERE c.userid=" + "'" + userid + "'" + " AND c.measureDate <= GetCurrentDateTime() ORDER BY c.measureDate DESC";
+        Iterator < FeedResponse < CosmosItemProperties >> responseIterator = container.queryItems(query, queryOptions);
+        responseIterator.forEachRemaining(response -> {
+            response.results().forEach(item -> {
                 SoilMeasurement sm = new SoilMeasurement(item.getString("id"), item.getString("userid"), item.getString("measureDate"), item.getDouble("nParam"), item.getDouble("pParam"), item.getDouble("kParam"), item.getDouble("pHParam"));
                 measurements.add(sm);
             });
@@ -61,12 +60,12 @@ public class DataProvider {
         return measurements.get(0);
     }
 
-    public List<Range> readRanges() {
-        List<Range> ranges = new ArrayList<>();
+    public List < Range > readRanges() {
+        List < Range > ranges = new ArrayList < > ();
         queryOptions.populateQueryMetrics(true);
         CosmosSyncContainer container = database.getContainer(bundle.getString("range"));
 
-        Iterator<FeedResponse<CosmosItemProperties>> responseIterator = container.queryItems("SELECT * FROM c", queryOptions);
+        Iterator < FeedResponse < CosmosItemProperties >> responseIterator = container.queryItems("SELECT * FROM c", queryOptions);
 
         responseIterator.forEachRemaining(response -> {
             response.results().forEach(item -> {
@@ -77,14 +76,14 @@ public class DataProvider {
         return ranges;
     }
 
-    public Price readPrice(){
-        List<Price> prices = new ArrayList<>();
+    public Price readPrice() {
+        List < Price > prices = new ArrayList < > ();
         queryOptions.populateQueryMetrics(true);
         CosmosSyncContainer container = database.getContainer(bundle.getString("price"));
         String query = "SELECT TOP 1 * FROM c WHERE c.updateDate <= GetCurrentDateTime() ORDER BY c.updateDate DESC";
-        Iterator<FeedResponse<CosmosItemProperties>> responseIterator = container.queryItems(query, queryOptions);
-        responseIterator.forEachRemaining( response -> {
-            response.results().forEach( item -> {
+        Iterator < FeedResponse < CosmosItemProperties >> responseIterator = container.queryItems(query, queryOptions);
+        responseIterator.forEachRemaining(response -> {
+            response.results().forEach(item -> {
                 Price p = new Price(item.getInt("id"), item.getString("updateDate"), item.getDouble("sweetpotatoPrice"), item.getDouble("maizePrice"), item.getDouble("soybeanPrice"));
                 prices.add(p);
             });
@@ -93,14 +92,14 @@ public class DataProvider {
     }
 
 
-    public List<Fertilizer> readFertilizer(){
-        List<Fertilizer> fertilizers = new ArrayList<>();
+    public List < Fertilizer > readFertilizer() {
+        List < Fertilizer > fertilizers = new ArrayList < > ();
         queryOptions.populateQueryMetrics(true);
         CosmosSyncContainer container = database.getContainer(bundle.getString("fertilizer"));
         String query = "SELECT * FROM c";
-        Iterator<FeedResponse<CosmosItemProperties>> responseIterator = container.queryItems(query, queryOptions);
-        responseIterator.forEachRemaining( response -> {
-            response.results().forEach( item -> {
+        Iterator < FeedResponse < CosmosItemProperties >> responseIterator = container.queryItems(query, queryOptions);
+        responseIterator.forEachRemaining(response -> {
+            response.results().forEach(item -> {
                 Fertilizer f = new Fertilizer(item.getInt("id"), item.getString("croptype"), item.getString("name"));
                 fertilizers.add(f);
             });
@@ -114,7 +113,7 @@ public class DataProvider {
     //endregion
 
     //region Update
-    public void updateSoilType(SoilMeasurement soilMeasurement) throws Exception{
+    public void updateSoilType(SoilMeasurement soilMeasurement) throws Exception {
         queryOptions.populateQueryMetrics(true);
         CosmosSyncContainer container = database.getContainer(bundle.getString("soilmeasurement"));
         container.upsertItem(soilMeasurement);
